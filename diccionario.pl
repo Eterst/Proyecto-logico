68b3a8e0bbc63c7155d67309b39e4aeac7a67153
Jonder <Jonder09@hotmail.com>
2020-11-06 12:54:21 -0600

    Añadido y arreglado decrem

--- diccionario.pl
+++ diccionario.pl
@@ -35,6 +35,14 @@ numero(7,[1,1,1,0,0,0,0]):-!.
 numero(8,[1,1,1,1,1,1,1]):-!.
 numero(9,[1,1,1,1,0,1,1]):-!.
 
+nsimo(1,[a,b,c,d,e,f,g],a).
+nsimo(2,[a,b,c,d,e,f,g],b).
+nsimo(3,[a,b,c,d,e,f,g],c).
+nsimo(4,[a,b,c,d,e,f,g],d).
+nsimo(5,[a,b,c,d,e,f,g],e).
+nsimo(6,[a,b,c,d,e,f,g],f).
+nsimo(7,[a,b,c,d,e,f,g],g).
+
 split([],_N,[],[]):-!.
 split(L,0,[],L):-!.
 split([H|T], N, [H|LS1],LS2):- N1 is N-1, split(T,N1,LS1,LS2).
@@ -44,13 +52,15 @@ dividir(L,X):- split(L,7,LS1,LS2), split(LS2,7,LS3,LS4), append([LS1],[LS3],LS5)
 mimap(_Rel, [],[]):-!.
 mimap(Rel,[H|T], [HR|TR]):- call(Rel,H,HR), mimap(Rel,T,TR).
 
+decimal([],[0,0,-1]):-!.
 decimal(L,LT2):- mimap(num,L,LT), dividir(LT,LD), mimap(numero, LT2, LD).
 
 decremental([A,B,C],[A2,B2,C2]):- SB2 is (B2*10)+C2, N2 is (A2*100)+SB2, SB is (B*10)+C, N1 is (A*100)+SB, N1 is N2+1,!.
 decremental(_L,[]):-!.
 
+car([],[]):-!.
 car([H|_T], H):-!.
 cdr([_H|T], T):-!.
 
 decrem([]):-!.
-decrem([H|T]):- decimal(H,DH), car(T,CT), decimal(CT,DT), decremental(DH,DT), decrem(T).
+decrem([H|T]):- decimal(H,DH), car(T,CT), decimal(CT,DT), decremental(DH,DT), !, decrem(T), !.
