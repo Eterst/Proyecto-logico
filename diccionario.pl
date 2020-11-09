82513ecb2776406b18eedda4eb40685e290b02d4
Juan Bautista <hidalgojb.23@gmail.com>
2020-11-09 15:32:48 -0600

    test

--- diccionario.pl
+++ diccionario.pl
@@ -46,22 +46,23 @@ mimap(_Rel, [],[]):-!.
 mimap(Rel,[H|T], [HR|TR]):- call(Rel,H,HR), mimap(Rel,T,TR).
 
 quemada([],_N,E):-!.
-quemada([H|T],N,E):- nsimo(N,H,E), quemada(T,N,E).
+quemada([H|T],N):- nsimo(N,H,78), quemada(T,N).
+
+decremental([A,B,C],[A2,B2,C2]):- SB2 is (B2*10)+C2, N2 is (A2*100)+SB2, SB is (B*10)+C, N1 is (A*100)+SB, N1 is N2+1,!.
+decremental(_L,[]):-!.
 
 %todo antes de este punto funciona
 
 encontrarDigitos(_LL,[],_N):-!.
 encontrarDigitos([LLH|LLT],[H|T], X):- encontrarDigitos([LLH|LLT], [H|T], 0, X).
 encontrarDigitos([LLH|LLT],[H|T], N, [HR|TR]):- call(num,LLT,N,H,HR), N1 is N+1, encontrarDigitos(LLT,T,N1,TR).
+%encontrar digitos siempre da no
 
 num(_LL,_N,89,1).
-num(_LL,_N,78,0). %hay un problema con 78 a veces solo entra para ser 0
-num(LL,N,78,1):- quemada(LL,N,78). % y me queda probar el conjunto de todo
-
-decimal(LL,L,LT2):- encontrarDigitos(LL,L,LT), dividir(LT,LD), mimap(numero, LT2, LD).
-
-decremental([A,B,C],[A2,B2,C2]):- SB2 is (B2*10)+C2, N2 is (A2*100)+SB2, SB is (B*10)+C, N1 is (A*100)+SB, N1 is N2+1,!.
-decremental(_L,[]):-!.
+num(_LL,_N,78,0).
+num(LL,N,78,1):- quemada(LL,N).
+%hay un problema con 78 a veces solo entra para ser 0, hay un fail en nsimo le esta llegando lista de lista [[hhhhect]].
+decimal(LL,L,LT2):- encontrarDigitos(LL,L,LT), dividir(LT,LD), mimap(numero, LT2, LD).%aun no pruebo
 
 decremento([H|[]]):-!.
-decremento([H|T]):- decimal(T,H,DH), car(T,CT), decimal(T, CT,DT), decremental(DH,DT), !, decremento(T), !.
+decremento([H|T]):- decimal(T,H,DH), car(T,CT), decimal(T, CT,DT), decremental(DH,DT), !, decremento(T), !.%aun no pruebo
